9c8044c02486cd580ebadd7b2f098bc5a9c2890f
OthmanAhmed7 <105116176+OthmanAhmed7@users.noreply.github.com>
2024-02-17 13:14:08 +0200

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -21,17 +21,19 @@ function App() {
   }
 
   return (
-    <main className="py-8 bg-container">
-      <div className="px-8 pt-8 pb-2 w-[375px]  md:w-[560px] m-auto shadow-2xl rounded-lg bg-white">
+    <main className="py-8 md:py-12 lg:py-16 bg-container">
+      <div className="px-8 pt-8 pb-2 w-[375px]  md:w-[560px] lg:w-[670px] m-auto shadow-2xl rounded-lg bg-white">
         <div className="flex items-center justify-between mb-8 text-sm">
           <div className="flex items-center gap-3">
-            <h1 className="text-lg font-bold text-names">Notification</h1>
-            <span className="px-3 rounded-lg bg-counter text-container">
+            <h1 className="text-lg font-bold lg:text-2xl text-names">
+              Notification
+            </h1>
+            <span className="px-3 rounded-lg lg:text-lg bg-counter text-container">
               {notification.filter((n) => n.isUnread).length}
             </span>
           </div>
           <p
-            className="cursor-pointer text-paragraph hover:text-counter"
+            className="cursor-pointer text-paragraph lg:text-base hover:text-counter"
             onClick={markAllUnread}
           >
             Mark all as read
@@ -48,13 +50,13 @@ function App() {
             >
               <div className="flex">
                 <img
-                  className="w-[40px] h-[40px] mr-3"
+                  className="w-[40px] lg:w-[50px] lg:h-[50px] h-[40px] mr-3"
                   src={n.imgUrl}
                   alt={n.title}
                 />
 
                 <div>
-                  <p className="text-xs">
+                  <p className="lg:text-[15px] text-xs">
                     <span className="font-bold cursor-pointer hover:text-counter">
                       {n.title}
                     </span>{" "}
@@ -65,13 +67,13 @@ function App() {
                       </span>
                     )}{" "}
                     {n.isUnread && (
-                      <span className="bg-mark block h-[10px] w-[10px] rounded-full"></span>
+                      <span className="bg-mark h-[8px] lg:h-[10px] lg:w-[10px] w-[8px] rounded-full block"></span>
                     )}
-                    <p className="text-xs text-time">{n.time}</p>
+                    <p className="text-xs lg:text-sm text-time">{n.time}</p>
                     <div
                       className={`${
                         n.message &&
-                        "border-2 p-2 rounded-md mt-3 max-w-[400px] hover:bg-background cursor-pointer"
+                        "border-2 p-2 rounded-md mt-3 lg:text-[15px] hover:bg-background cursor-pointer"
                       }`}
                     >
                       {n.message && (
@@ -80,11 +82,12 @@ function App() {
                     </div>
                   </p>
                 </div>
+
                 {n.picture && (
                   <img
                     src={n.picture}
                     alt="picture"
-                    className="h-[38px] w-[38px] ml-3 cursor-pointer"
+                    className="h-[38px] w-[38px] lg:h-[44px] lg:w-[44px] ml-3 cursor-pointer"
                   />
                 )}
               </div>
