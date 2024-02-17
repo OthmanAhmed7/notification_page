22f9f54abf313a41c20c186e8f84e6b3bd92d983
OthmanAhmed7 <105116176+OthmanAhmed7@users.noreply.github.com>
2024-02-17 13:16:55 +0200

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,9 @@ export default {
       colors: {
         mark: "hsl(1, 90%, 64%)",
         counter: "hsl(219, 85%, 26%)",
-        container: "hsl(0, 0%, 100%)",
+        container: "hsl(228, 71%, 99%)",
         background: "hsl(211, 68%, 94%)",
-        notification: "hsl(205, 33%, 90%)",
+        notification: "hsl(206, 64%, 98%)",
         time: "hsl(219, 14%, 63%)",
         paragraph: "hsl(219, 12%, 42%)",
         names: "hsl(224, 21%, 14%)",
